05c414bfd26f1924ba2dd4e133f4df7e86af217e
Jack Kamm <jackkamm@gmail.com>
2018-04-10 17:07:13 +0100

    evil org-mode bindings

--- layers/org.el
+++ layers/org.el
@@ -1,3 +1,13 @@
+(use-package evil-org
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
 (setq org-agenda-files (list "~/Dropbox/org/agenda.org"))
 (setq org-capture-templates
     '(("t" "todo" entry (file "~/Dropbox/org/agenda.org")
