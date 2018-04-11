8fbe81839909486f72414d1e8e83e54d381953df
Jack Kamm <jackkamm@gmail.com>
2018-04-11 18:03:03 +0100

    theme

--- layers/theme.el
+++ layers/theme.el
@@ -3,18 +3,29 @@
 (tool-bar-mode -1) 
 (blink-cursor-mode -1)
 
-(use-package solarized-theme
-  :config
-  (load-theme 'solarized-dark t)
-  (defun load-solarized-light () (interactive)
-	 (load-theme 'solarized-light))
-  (defun load-solarized-dark () (interactive)
-	 (load-theme 'solarized-dark))
-  (my-leader
-    "t" '(:ignore t :which-key "theme")
-    "tt" 'load-theme
-    "tl" 'load-solarized-light
-    "td" 'load-solarized-dark))
+(use-package material-theme :defer t)
+
+(setq my-light-theme 'leuven)
+(setq my-dark-theme 'material)
+
+(defun load-light-theme () (interactive)
+       (disable-theme my-dark-theme)
+       (load-theme my-light-theme t)
+       (evil-refresh-cursor))
+(defun load-dark-theme () (interactive)
+       (disable-theme my-light-theme)
+       (load-theme my-dark-theme t)
+       (evil-refresh-cursor))
+
+;;(load-light-theme)
+;;(load-material)
+(load-dark-theme)
+
+(my-leader
+  "t" '(:ignore t :which-key "theme")
+  "tt" 'load-theme
+  "tl" 'load-light-theme
+  "td" 'load-dark-theme)
 
 ;; evil cursor colors
 (setq evil-normal-state-cursor "orange"
