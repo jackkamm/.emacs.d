460ad55bdb9676677dac76acd23b1fd79b45b775
Jack Kamm <jackkamm@gmail.com>
2018-04-13 14:05:01 +0100

    org babel macro

--- layers/org-babel.el
+++ layers/org-babel.el
@@ -9,4 +9,13 @@
      (python . t)
      (emacs-lisp . t)
      (shell . t)
-     (ipython . t))))
+     (ipython . t)))
+  ;; macro for "C-c ' M-m e b C-c '"
+  ;; opens a babel block (C-c '), executes the buffer (M-m e b), closes the babel block (C-c ')
+  ;; assumes convention (M-m e b) for executing buffer defined across modes
+  (fset 'babel-async-eval-session
+	(lambda (&optional arg) "Keyboard macro." (interactive "p")
+	  (kmacro-exec-ring-item (quote ("'\355eb'" 0 "%d")) arg)))
+  (my-eval-leader
+    :keymaps 'org-mode-map
+    "s" 'babel-async-eval-session))
