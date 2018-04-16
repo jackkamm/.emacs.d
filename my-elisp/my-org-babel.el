8cc196e202d32f4afce3f4269af4393bba79a79f
Jack Kamm <jackkamm@gmail.com>
2018-04-16 13:40:46 +0100

    org-babel bindings

--- my-elisp/my-org-babel.el
+++ my-elisp/my-org-babel.el
@@ -13,10 +13,17 @@
   ;; macro for "C-c ' M-m e b C-c '"
   ;; opens block (C-c '), evals buffer (M-m e b), closes block (C-c ')
   ;; assumes convention (M-m e b) to eval buffer defined across modes
-  (fset 'babel-async-eval-session
+  (fset 'my-babel-async-eval-session
 	(lambda (&optional arg) "Keyboard macro." (interactive "p")
 	  (kmacro-exec-ring-item
 	   (quote ("'\355eb'" 0 "%d")) arg)))
-  (my-eval-leader
+  (defhydra my-babel-hydra ()
+    "babel"
+    ("n" org-babel-next-src-block "next")
+    ("p" org-babel-previous-src-block "previous")
+    ("s" my-babel-async-eval-session "send-session")
+    ("x" org-babel-execute-maybe "execute")
+    ("q" nil "quit"))
+  (my-major-leader
     :keymaps 'org-mode-map
-    "s" 'babel-async-eval-session))
+    "b" 'my-babel-hydra/body))
