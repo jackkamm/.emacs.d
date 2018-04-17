ac1ea185ce8354c7c75e97f7edd41345d93c238a
Jack Kamm <jackkamm@gmail.com>
2018-04-17 14:04:58 +0100

    git timemachine keys

--- my-elisp/my-git.el
+++ my-elisp/my-git.el
@@ -8,4 +8,9 @@
 (use-package git-timemachine
   :bind
   (:map my-leader-map
-	("g t" . git-timemachine)))
+	("g t" . git-timemachine))
+  :config
+  (add-hook 'git-timemachine-mode-hook 'evil-motion-state)
+  (evil-define-key 'motion git-timemachine-mode-map
+    (kbd "C-n") 'git-timemachine-show-next-revision
+    (kbd "C-p") 'git-timemachine-show-previous-revision))
