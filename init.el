e0ca82a5f00e3495bdfb0b3478ef5a6961fc3761
Jack Kamm <jackkamm@gmail.com>
2018-04-19 08:05:45 +0100

    refactoring, added lisp/ dir

--- init.el
+++ init.el
@@ -1,93 +1,102 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
-
 (let ((init-start-time (float-time)))
-  (defun my-load-elisp (module-path)
-    (let ((module-init-start (float-time)))
+  ;; setup package.el, load-path
+  (let ((package-init-start (float-time)))
+    (require 'package)
+    (setq package-enable-at-startup nil)
+    (add-to-list 'package-archives
+		 '("melpa" . "https://melpa.org/packages/"))
+
+    (package-initialize)
+
+    (let ((default-directory  (concat user-emacs-directory "lisp/")))
+      (normal-top-level-add-to-load-path '("."))
+      (normal-top-level-add-subdirs-to-load-path))
+
+    (message
+     (format "Initialized packages, load-path in %.2f seconds."
+	     (- (float-time) package-init-start))))
+
+  ;; function to load layer, time it, catch any errors
+  (defun my-load-layer (layer-path)
+    (let ((layer-init-start (float-time)))
       (condition-case err
 	  (progn
-	    (load (expand-file-name module-path
+	    (load (expand-file-name layer-path
 				    user-emacs-directory)
 		  nil t)
 	    (message
-	     (concat "Loaded " module-path " in "
+	     (concat "Loaded " layer-path " in "
 		     (format "%.2f" (- (float-time)
-				       module-init-start))
+				       layer-init-start))
 		     " seconds.")))
 	(error
 	 (display-warning :error
 			  (concat "Error loading "
-				  module-path ": "
+				  layer-path ": "
 				  (error-message-string err)))))))
 
-  (setq my-elisp-modules
+  (setq my-layers
 	(list
 	 ;;;; MUST be loaded first!
-	 "my-elisp/my-use-package.el"
-	 "my-elisp/my-keys.el" ;evil, general, which-key, bind-key
-	 "my-elisp/my-org.el" ;ensure org-plus-contrib
+	 "my-layers/my-use-package.el"
+	 "my-layers/my-keys.el" ;evil, general, which-key, bind-key
+	 "my-layers/my-org.el" ;ensure org-plus-contrib
 	 ;; other core packages
-	 "my-elisp/my-helm.el"
-	 "my-elisp/my-hydra.el"
+	 "my-layers/my-helm.el"
+	 "my-layers/my-hydra.el"
 
 	 ;; editing
-	 ;;"my-elisp/my-hybrid-state.el"
-	 "my-elisp/my-jump.el" ;SPC-j: avy, easymotion, xref, etc
-	 "my-elisp/my-multiedit.el" ;iedit, multicursor
-	 "my-elisp/my-parens.el" ;smartparens, evil-surround
-	 "my-elisp/my-snippets.el"
-	 "my-elisp/my-flycheck.el"
-	 "my-elisp/my-company.el"
-	 "my-elisp/my-expand-region.el"
-	 "my-elisp/my-scroll-hydra.el"
-	 "my-elisp/my-undo-tree.el"
+	 ;;"my-layers/my-hybrid-state.el"
+	 "my-layers/my-jump.el" ;SPC-j: avy, easymotion, xref, etc
+	 "my-layers/my-multiedit.el" ;iedit, multicursor
+	 "my-layers/my-parens.el" ;smartparens, evil-surround
+	 "my-layers/my-snippets.el"
+	 "my-layers/my-flycheck.el"
+	 "my-layers/my-company.el"
+	 "my-layers/my-expand-region.el"
+	 "my-layers/my-scroll-hydra.el"
+	 "my-layers/my-undo-tree.el"
 
 	 ;; applications
-	 "my-elisp/my-git.el"
-	 "my-elisp/my-email.el"
-	 "my-elisp/my-irc.el"
-	 "my-elisp/my-ediff.el"
+	 "my-layers/my-git.el"
+	 "my-layers/my-email.el"
+	 "my-layers/my-irc.el"
+	 "my-layers/my-ediff.el"
 
 	 ;; theming
-	 "my-elisp/my-theme.el"
-	 "my-elisp/my-window-layout.el"
-	 "my-elisp/my-linum.el"
-	 "my-elisp/my-hidpi.el"
-	 "my-elisp/my-prompts.el"
-	 "my-elisp/my-pos-tip.el"
-	 "my-elisp/my-highlight.el"
-	 "my-elisp/my-popwin.el"
-	 "my-elisp/my-saving.el"
-	 "my-elisp/my-minimap.el"
-	 "my-elisp/my-default-browser.el"
+	 "my-layers/my-theme.el"
+	 "my-layers/my-window-layout.el"
+	 "my-layers/my-linum.el"
+	 "my-layers/my-hidpi.el"
+	 "my-layers/my-prompts.el"
+	 "my-layers/my-pos-tip.el"
+	 "my-layers/my-highlight.el"
+	 "my-layers/my-popwin.el"
+	 "my-layers/my-saving.el"
+	 "my-layers/my-minimap.el"
+	 "my-layers/my-default-browser.el"
 
 	 ;; languages
-	 "my-elisp/my-python.el"
-	 "my-elisp/my-ein.el"
-	 "my-elisp/my-ess.el"
-	 "my-elisp/my-emacs-lisp.el"
-	 "my-elisp/my-org-babel.el"
-	 "my-elisp/my-tex.el"
-	 "my-elisp/my-web.el"
+	 "my-layers/my-python.el"
+	 "my-layers/my-ein.el"
+	 "my-layers/my-ess.el"
+	 "my-layers/my-emacs-lisp.el"
+	 "my-layers/my-org-babel.el"
+	 "my-layers/my-tex.el"
+	 "my-layers/my-web.el"
 	 ;; c-c++: only enable 1 of cquery, rtags
-	 "my-elisp/my-cquery.el"
-	 ;;"my-elisp/my-rtags.el"
+	 "my-layers/my-cquery.el"
+	 ;;"my-layers/my-rtags.el"
 
 	 ;; miscellaneous
-	 "my-elisp/my-start-server.el"
-	 "my-elisp/my-set-custom-file.el"
-	 "my-elisp/my-tramp.el"
+	 "my-layers/my-start-server.el"
+	 "my-layers/my-set-custom-file.el"
+	 "my-layers/my-tramp.el"
 	 ))
 
-  (mapcar 'my-load-elisp my-elisp-modules)
+  (mapcar 'my-load-layer my-layers)
 
   (message
-   (concat "Startup took "
-	   (format "%.2f" (- (float-time)
-			     init-start-time))
-	   " seconds.")))
+   (format "Startup took %.2f seconds."
+	   (- (float-time) init-start-time))))
 
---
+++ my-layers/my-use-package.el
@@ -0,0 +1,9 @@
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; always install missing packages
+(setq use-package-always-ensure t)
