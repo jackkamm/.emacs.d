75752838f61c6fd77329c4a93085aa3a13d7004e
Jack Kamm <jackkamm@gmail.com>
2018-04-19 14:29:15 +0100

    ob-ipython snippet

--- lisp/my-config/my-snippets.el
+++ lisp/my-config/my-snippets.el
@@ -1,6 +1,7 @@
 (use-package yasnippet
   :init
   (add-hook 'prog-mode-hook 'yas-minor-mode-on)
+  (add-hook 'text-mode-hook 'yas-minor-mode-on)
   :commands yas-minor-mode-on)
 
 (use-package helm-c-yasnippet
@@ -8,7 +9,6 @@
 	     helm-yas-visit-snippet-file)
   :general
   (my-leader
-    :keymaps 'prog-mode-map
     "is" 'helm-yas-complete))
 
 (use-package yasnippet-snippets
