d26cae8bc4dbbff8198081c6422ad84d4a88bcb6
Jack Kamm <jackkamm@gmail.com>
2018-04-20 13:05:51 +0100

    iedit

--- lisp/my-config/my-multiedit.el
+++ lisp/my-config/my-multiedit.el
@@ -1,5 +1,19 @@
 (use-package iedit
-  :bind ("C-;" . 'iedit-mode))
+  :general
+  (my-leader
+    "se" 'iedit-mode)
+  :config
+  ;; modifying iedit keymaps with evil has weird side effects,
+  ;; so create a new minor-mode
+  (define-minor-mode my-iedit-mode nil :keymap '())
+  (evil-define-minor-mode-key 'normal 'my-iedit-mode
+    ;; TODO add more bindings
+    "n" 'iedit-next-occurrence
+    "N" 'iedit-prev-occurrence)
+  (defun my-iedit-mode-on () (interactive) (my-iedit-mode 1))
+  (defun my-iedit-mode-off () (interactive) (my-iedit-mode 0))
+  (add-hook 'iedit-mode-hook 'my-iedit-mode-on)
+  (add-hook 'iedit-mode-end-hook 'my-iedit-mode-off))
 
 ;;(global-unset-key (kbd "S-<down-mouse-1>"))
 (use-package evil-mc
