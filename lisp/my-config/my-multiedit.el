81aef80df2d52ea328d0db071acdbd3ed11cf285
Jack Kamm <jackkamm@gmail.com>
2018-04-20 14:29:39 +0100

    iedit keys

--- lisp/my-config/my-multiedit.el
+++ lisp/my-config/my-multiedit.el
@@ -3,13 +3,27 @@
   (my-leader
     "se" 'iedit-mode)
   :config
-  ;; modifying iedit keymaps with evil has weird side effects,
+  ;; evil-define on iedit keymaps has side effects(?),
   ;; so create a new minor-mode
   (define-minor-mode my-iedit-mode nil :keymap '())
   (evil-define-minor-mode-key 'normal 'my-iedit-mode
-    ;; TODO add more bindings
+    (kbd "<escape>") 'iedit-quit
     "n" 'iedit-next-occurrence
-    "N" 'iedit-prev-occurrence)
+    "N" 'iedit-prev-occurrence
+    (kbd "/ f") 'iedit-restrict-function
+    (kbd "/ v") 'iedit-show/hide-unmatched-lines
+    (kbd "/ /") 'iedit-toggle-selection
+    (kbd "RET") 'iedit-toggle-selection
+    (kbd "/ n") 'iedit-expand-down-to-occurrence
+    (kbd "C-j") 'iedit-expand-down-to-occurrence
+    (kbd "/ N") 'iedit-expand-up-to-occurrence
+    (kbd "C-k") 'iedit-expand-up-to-occurrence
+    (kbd "/ l") 'iedit-restrict-current-line
+    (kbd "M-l") 'iedit-restrict-current-line
+    (kbd "/ j") 'iedit-expand-down-a-line
+    (kbd "M-j") 'iedit-expand-down-a-line
+    (kbd "/ k") 'iedit-expand-up-a-line
+    (kbd "M-k") 'iedit-expand-up-a-line)
   (defun my-iedit-mode-on () (interactive) (my-iedit-mode 1))
   (defun my-iedit-mode-off () (interactive) (my-iedit-mode 0))
   (add-hook 'iedit-mode-hook 'my-iedit-mode-on)
@@ -23,4 +37,5 @@
   (dolist (h (list 'prog-mode-hook
 		   'text-mode-hook))
     (add-hook h 'turn-on-evil-mc-mode)))
+
 
