5149b7f93183f5d2f3c17fa9b6b467b2dd92e4f3
Jack Kamm <jackkamm@gmail.com>
2018-05-02 14:02:42 -0700

    fixed snippets

--- lisp/my-config/my-snippets.el
+++ lisp/my-config/my-snippets.el
@@ -2,7 +2,9 @@
   :init
   (add-hook 'prog-mode-hook 'yas-minor-mode-on)
   (add-hook 'text-mode-hook 'yas-minor-mode-on)
-  :commands yas-minor-mode-on)
+  :commands yas-minor-mode-on
+  :config
+  (yas-reload-all))
 
 (with-eval-after-load "my-helm"
   (use-package helm-c-yasnippet
@@ -12,5 +14,4 @@
     (my-leader
       "is" 'helm-yas-complete)))
 
-(use-package yasnippet-snippets
-  :defer t)
+(use-package yasnippet-snippets :defer t)
