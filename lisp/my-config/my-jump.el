32237af17b921acd57effae6fb53d8d3fe414ec9
Jack Kamm <jackkamm@gmail.com>
2018-05-11 16:27:15 +0100

    my-goto stuff

--- lisp/my-config/my-jump.el
+++ lisp/my-config/my-jump.el
@@ -1,28 +1,28 @@
 (general-define-key
- :states '(normal motion)
+ :states '(normal motion visual)
  ;; inverse of "gd" evil-goto-definition
  "gD" 'xref-find-references)
 
 (my-leader
-  "g" '(:ignore t :which-key "Jump"))
+  "g" '(:ignore t :which-key "Goto"))
 (general-create-definer
-  my-jump-leader
+  my-goto-leader
   :prefix "C-c g")
 (general-define-key
  :keymaps 'override
  :states '(normal motion visual)
- "g SPC" (general-simulate-key "C-c g" :which-key "Jump"))
+ "g SPC" (general-simulate-key "C-c g" :which-key "my-motions"))
 
 (use-package avy
   :general
-  (my-jump-leader
+  (my-goto-leader
     "s" 'avy-isearch
-    "j"  'avy-goto-char-timer
+    "g"  'avy-goto-char-timer
     "w"  'avy-goto-word-1
     "l"  'avy-goto-line))
 
 (use-package evil-easymotion
   :config
-  (evilem-default-keybindings "C-c g SPC")
-  (my-jump-leader
+  (evilem-default-keybindings "C-c g e")
+  (my-goto-leader
     "SPC" '(:ignore-key t :which-key "easymotion")))
