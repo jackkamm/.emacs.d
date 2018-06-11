b27e17ef22d28be8a00acd61af2ded100eb20bff
Jack Kamm <jackkamm@gmail.com>
2018-06-11 11:15:57 -0700

    only use linum on prog-mode (text-modes are in visual-line-mode)

--- lisp/my-config/my-theme.el
+++ lisp/my-config/my-theme.el
@@ -36,15 +36,7 @@
 (use-package linum-relative
   :config
   (setq linum-relative-current-symbol "")
-  ;;(linum-relative-global-mode 1)
-  (define-globalized-minor-mode
-    my-global-linum-relative-mode
-    linum-relative-mode
-    (lambda ()
-      (when (derived-mode-p 'prog-mode 'text-mode)
-	(unless (memq major-mode (list 'org-mode))
-	    (linum-relative-mode 1)))))
-  (my-global-linum-relative-mode 1))
+  (add-hook 'prog-mode-hook 'linum-relative-on))
 
 ;;; theme
 
