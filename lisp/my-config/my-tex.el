8d745ea2079227c0ae9a8e8340f84203faf797b4
Jack Kamm <jackkamm@gmail.com>
2018-06-13 08:14:31 -0700

    cleanup

--- lisp/my-config/my-tex.el
+++ lisp/my-config/my-tex.el
@@ -9,13 +9,6 @@
   :config
   (TeX-source-correlate-mode))
 
-;; helm \includegraphics looks in local directory,
-;; instead of TeX search path
+;; start helm \includegraphics in local directory
 (setq LaTeX-includegraphics-read-file
       'LaTeX-includegraphics-read-file-relative)
-
-;; set okular as default pdf viewer in linux
-(with-eval-after-load 'tex
-  (when (eq system-type 'gnu/linux)
-          (add-to-list 'TeX-view-program-selection
-                       '(output-pdf "Okular"))))
