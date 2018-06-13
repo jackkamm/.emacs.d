64638fad361fe79de7e5726a4742d8db8a2d4db4
Jack Kamm <jackkamm@gmail.com>
2018-06-13 08:12:31 -0700

    web-mode not needed in emacs>=26

--- lisp/my-config/my-web.el
+++ lisp/my-config/my-web.el
@@ -1,5 +1,6 @@
-(use-package web-mode
-  :mode
-  (("\\.html\\'"       . web-mode)
-   ("\\.htm\\'"        . web-mode)
-   ("\\.xml\\'"        . web-mode)))
+(if (version< emacs-version "26")
+    (use-package web-mode
+      :mode
+      (("\\.html\\'"       . web-mode)
+       ("\\.htm\\'"        . web-mode)
+       ("\\.xml\\'"        . web-mode))))
