fae3e2e2ad57e6766b81a5315adfada65c6f2335
Jack Kamm <jackkamm@gmail.com>
2018-06-16 06:29:20 -0700

    my-apps: remove setting browse-url

--- lisp/my-config/my-apps.el
+++ lisp/my-config/my-apps.el
@@ -1,8 +1,3 @@
-;;; default browser
-
-;; https://lists.gnu.org/archive/html/emacs-devel/2014-11/msg00328.html
-(setq browse-url-browser-function 'browse-url-xdg-open)
-
 ;;; open external terminal
 
 (defun my-tmux-open
