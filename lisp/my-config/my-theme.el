16ed59d18ea4540a842baeaaa94ee4a37893739a
Jack Kamm <jackkamm@gmail.com>
2018-07-06 15:49:57 -0700

    use linum in conf-mode

--- lisp/my-config/my-theme.el
+++ lisp/my-config/my-theme.el
@@ -30,7 +30,8 @@
 (use-package linum-relative
   :config
   (setq linum-relative-current-symbol "")
-  (add-hook 'prog-mode-hook 'linum-relative-on))
+  (add-hook 'prog-mode-hook 'linum-relative-on)
+  (add-hook 'conf-mode-hook 'linum-relative-on))
 
 ;; line wrap in text modes
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
