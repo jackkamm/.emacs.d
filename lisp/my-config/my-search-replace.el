e98c278afd99581a419eb9de76528c6973fc2e8a
Jack Kamm <jackkamm@gmail.com>
2018-09-16 08:28:23 -0700

    switch to evil-iedit-state

--- lisp/my-config/my-search-replace.el
+++ lisp/my-config/my-search-replace.el
@@ -52,36 +52,10 @@
 
 ;;; iedit
 
-(use-package iedit
+(use-package evil-iedit-state
   :general
   (my-search-replace-leader
-    "e" 'iedit-mode)
-  :config
-  ;; evil-define on iedit modes/maps has strange effects(?)
-  ;; so create a new minor-mode instead
-  (define-minor-mode my-iedit-mode nil :keymap '())
-  (add-hook 'iedit-mode-hook #'evil-normalize-keymaps)
-  (add-hook 'iedit-mode-hook #'my-iedit-mode)
-  (add-hook 'iedit-mode-end-hook (lambda () (my-iedit-mode -1)))
-  (evil-define-key 'normal 'my-iedit-mode
-    (kbd "<escape>") 'iedit-quit
-    "n" 'iedit-next-occurrence
-    "N" 'iedit-prev-occurrence
-    "D" 'iedit-delete-occurrences
-    (kbd "/ f") 'iedit-restrict-function
-    (kbd "/ v") 'iedit-show/hide-unmatched-lines
-    (kbd "/ /") 'iedit-toggle-selection
-    (kbd "RET") 'iedit-toggle-selection
-    (kbd "/ n") 'iedit-expand-down-to-occurrence
-    (kbd "C-j") 'iedit-expand-down-to-occurrence
-    (kbd "/ N") 'iedit-expand-up-to-occurrence
-    (kbd "C-k") 'iedit-expand-up-to-occurrence
-    (kbd "/ l") 'iedit-restrict-current-line
-    (kbd "M-l") 'iedit-restrict-current-line
-    (kbd "/ j") 'iedit-expand-down-a-line
-    (kbd "M-j") 'iedit-expand-down-a-line
-    (kbd "/ k") 'iedit-expand-up-a-line
-    (kbd "M-k") 'iedit-expand-up-a-line))
+    "e" 'evil-iedit-state/iedit-mode))
 
 ;;; evil-mc
 
