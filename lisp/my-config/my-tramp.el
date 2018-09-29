9cc0edfa764e8cb48d8b69d573c6588c9a89d35f
Jack Kamm <jackkamm@gmail.com>
2018-09-29 15:49:50 -0700

    ssh-agent forwarding

--- lisp/my-config/my-tramp.el
+++ lisp/my-config/my-tramp.el
@@ -3,8 +3,15 @@
 				    vc-ignore-dir-regexp
 				    tramp-file-name-regexp))
 
-;; use correct path when executing code block in remote :dir
-;; or using eshell
 (with-eval-after-load 'tramp-sh
-  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+  ;; use correct path when executing code block in remote :dir
+  ;; or using eshell
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+  ;; use ssh-agent forwarding for /ssh: connections
+  ;; this has security implications; use /scp: to avoid forwarding
+  (let* ((ssh-args (assoc "ssh" tramp-methods))
+	 (ssh-login-list (assoc 'tramp-login-args ssh-args))
+	 (ssh-login-args (car (cdr ssh-login-list))))
+    (setcdr ssh-login-list (add-to-list 'ssh-login-args '("-A")))))
 
