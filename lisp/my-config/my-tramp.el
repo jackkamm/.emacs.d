ae728703c1a981536cbfc2d307d1ab5fb15bc162
Jack Kamm <jackkamm@gmail.com>
2018-09-29 17:32:24 -0700

    remove ssh-agent stuff from tramp

--- lisp/my-config/my-tramp.el
+++ lisp/my-config/my-tramp.el
@@ -6,14 +6,5 @@
 (with-eval-after-load 'tramp-sh
   ;; use correct path when executing code block in remote :dir
   ;; or using eshell
-  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
-
-  ;; use ssh-agent forwarding under /scp:
-  ;; this has security implications; use /ssh: to avoid forwarding
-  ;; https://emacs.stackexchange.com/questions/18262/tramp-how-to-add-a-agent-forwarding-to-ssh-connections/18280
-  (add-to-list 'tramp-connection-properties
-               (list (regexp-quote "/scp:")
-                     "login-args"
-                     '(("-A") ("-l" "%u") ("-p" "%p") ("%c")
-                       ("-e" "none") ("%h")))))
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
 
