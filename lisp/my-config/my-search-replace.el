e7a65b529691eae29e0cc035e59125a19e01aa7d
Jack Kamm <jackkamm@gmail.com>
2018-10-13 19:30:39 -0700

    switch from evil-mc to multiple-cursors (emacs-state)

--- lisp/my-config/my-search-replace.el
+++ lisp/my-config/my-search-replace.el
@@ -51,44 +51,12 @@
   (my-search-replace-leader
     "e" 'evil-iedit-state/iedit-mode))
 
-;;; evil-mc
+;; multiple-cursors
 
-(use-package evil-mc
-  :commands my-evil-mc-hydra/evil-mc-toggle-cursor-on-click
-  :bind (("S-<down-mouse-1>" . 'my-evil-mc-hydra/evil-mc-toggle-cursor-on-click))
-  :general
-  (my-search-replace-leader
-    "m" 'my-evil-mc-hydra/body)
-  :init
-  (setq evil-mc-key-map nil)
+(use-package multiple-cursors
+  :bind (("S-<down-mouse-1>" . nil)
+	 ("S-<mouse-1>" . 'mc/add-cursor-on-click))
   :config
-  (global-evil-mc-mode)
-  (require 'hydra)
-  (defhydra my-evil-mc-hydra ()
-    "multicursor"
-    ("S-<down-mouse-1>" evil-mc-toggle-cursor-on-click)
-    ("u" evil-mc-undo-all-cursors "remove-all" :exit t)
-    ("m" evil-mc-make-all-cursors "make-all")
-    ("p" my-paused-mc-hydra/body "pause" :exit t)
-    ("J" evil-mc-make-cursor-move-next-line "next-line")
-    ("K" evil-mc-make-cursor-move-prev-line "prev-line")
-    ("H" evil-mc-make-and-goto-first-cursor "swap-to-first")
-    ("L" evil-mc-make-and-goto-last-cursor "swap-to-last")
-    ("C-j"  evil-mc-make-and-goto-next-cursor "swap-forward")
-    ("C-k"  evil-mc-make-and-goto-prev-cursor "swap-backward")
-    ("M-j" evil-mc-skip-and-goto-next-cursor "pop-forward")
-    ("M-k" evil-mc-skip-and-goto-prev-cursor "pop-backward")
-    ("C-n"  evil-mc-make-and-goto-next-match "make-next")
-    ("C-p"  evil-mc-make-and-goto-prev-match "make-prev")
-    ("M-n" evil-mc-skip-and-goto-next-match "pop-to-next-match")
-    ("M-p" evil-mc-skip-and-goto-prev-match "pop-to-prev-match"))
-  (defhydra my-paused-mc-hydra (nil nil
-				    :foreign-keys run
-				    :body-pre (evil-mc-pause-cursors)
-				    :post (evil-mc-resume-cursors))
-    "paused-multicursor"
-    ("S-<down-mouse-1>" evil-mc-toggle-cursor-on-click)
-    ("<RET>" evil-mc-make-cursor-here "mark-here")
-    ("r" my-evil-mc-hydra/body "resume" :exit t)
-    ("a" evil-append :exit t)
-    ("i" evil-insert :exit t)))
+  (add-hook 'multiple-cursors-mode-enabled-hook 'evil-emacs-state)
+  (add-hook 'multiple-cursors-mode-disabled-hook 'evil-exit-emacs-state)
+  (define-key mc/keymap (kbd "<return>") nil))
