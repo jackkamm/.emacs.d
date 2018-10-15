045b9f3b17460cc6c0c56d69e0ac653e5c6fabb8
Jack Kamm <jackkamm@gmail.com>
2018-10-15 07:41:16 -0700

    evil-matchit

--- lisp/my-config/my-parens.el
+++ lisp/my-config/my-parens.el
@@ -6,3 +6,14 @@
   :config
   (require 'smartparens-config)
   (smartparens-global-mode))
+
+(use-package evil-matchit
+  :config
+  (global-evil-matchit-mode 1)
+  ;; access the "raw" default evil-jump
+  (my-leader
+    "%" 'evil-jump-item)
+  ;; mhtml-mode
+  (plist-put evilmi-plugins 'mhtml-mode '((evilmi-template-get-tag evilmi-template-jump)
+                                          (evilmi-simple-get-tag evilmi-simple-jump)
+                                          (evilmi-html-get-tag evilmi-html-jump))))
