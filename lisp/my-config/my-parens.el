e8ff44b5f6483f00b29cadcf00628386a22dc761
Jack Kamm <jackkamm@gmail.com>
2018-12-08 08:40:17 -0800

    lispy(ville)

--- lisp/my-config/my-parens.el
+++ lisp/my-config/my-parens.el
@@ -16,3 +16,26 @@
              '((evilmi-template-get-tag evilmi-template-jump)
                (evilmi-simple-get-tag evilmi-simple-jump)
                (evilmi-html-get-tag evilmi-html-jump))))
+
+(use-package lispy
+  :commands lispy-mode
+  :init
+  (add-hook 'emacs-lisp-mode-hook 'lispy-mode)
+  (add-hook 'clojure-mode-hook 'lispy-mode)
+  :config
+  ;; disables the special (location) key-theme
+  (lispy-set-key-theme '(lispy c-digits)))
+
+(use-package lispyville
+  ;:after (lispy evil)
+  :config
+  (add-hook 'lispy-mode-hook #'lispyville-mode)
+  (lispyville-set-key-theme
+   '(operators
+     c-w
+     escape
+     wrap
+     slurp/barf-lispy
+     additional-movement
+     additional-wrap)))
+
