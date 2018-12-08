cebbb87de260f80170127ef18d31d6669a7ea349
Jack Kamm <jackkamm@gmail.com>
2018-12-08 08:15:28 -0800

    disable undo-tree

--- lisp/my-config/my-undo.el
+++ lisp/my-config/my-undo.el
@@ -1,5 +1,10 @@
-(use-package undo-tree
-  :general
-  (my-leader "_" 'undo-tree-visualize)
-  :init
-  (setq undo-tree-enable-undo-in-region nil))
+;; disable undo-tree because of it's awful bug
+;; https://www.reddit.com/r/emacs/comments/85t95p/undo_tree_unrecognized_entry_in_undo_list/
+(with-eval-after-load 'evil
+  (global-undo-tree-mode -1))
+
+;;(use-package undo-tree
+;;  :general
+;;  (my-leader "_" 'undo-tree-visualize)
+;;  :init
+;;  (setq undo-tree-enable-undo-in-region nil))
