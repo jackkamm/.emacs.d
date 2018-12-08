c95530ce55c5ea371dd4c74220cf47d88617fa6b
Jack Kamm <jackkamm@gmail.com>
2018-12-08 08:26:26 -0800

    evil-magit

--- lisp/my-config/my-git.el
+++ lisp/my-config/my-git.el
@@ -3,9 +3,12 @@
 (use-package magit
   :general
   (my-leader
-    "gs" 'magit-status)
-  :config
-  (use-package evil-magit))
+    "gs" 'magit-status))
+
+(use-package evil-magit
+  :after (evil magit)
+  :init
+  (setq evil-magit-want-horizontal-movement t))
 
 (use-package git-timemachine
   :general
