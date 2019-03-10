97329fefc2093f3c8de9b8104336ec94ab4f4c01
jackkamm@gmail.com <jackkamm@gmail.com>
2019-03-09 21:23:23 -0800

    Add markdown-mode

--- lisp/my-config/my-lang-misc.el
+++ lisp/my-config/my-lang-misc.el
@@ -13,3 +13,11 @@
 	 ("\\.\\(?:sm\\)?rules\\'" . snakemake-mode)
 	 ("\\.smk\\'" . snakemake-mode)
 	 ("\\.snakefile\\'" . snakemake-mode)))
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc"))
