f606761bbb1d167cd6a9d4f284c031de5510a5ee
jackkamm@gmail.com <jackkamm@gmail.com>
2019-03-12 22:39:13 -0700

    Simplify email config

--- lisp/my-config/my-email.el
+++ lisp/my-config/my-email.el
@@ -1,24 +1,10 @@
 (setq message-send-mail-function 'message-send-mail-with-sendmail)
 (setq message-make-forward-subject-function 'message-forward-subject-fwd)
 
-(defun my-inbox (fetch-calendar-p)
-  (interactive
-   (list (y-or-n-p "Fetch google calendar?")))
-  (if fetch-calendar-p
-      (progn
-	(require 'my-org-gcal)
-	(org-gcal-fetch)))
-  (notmuch-search "tag:unread")
-  (let ((nm-window (get-buffer-window)))
-    (org-agenda nil "n")
-    (select-window nm-window)))
-
 (use-package notmuch
-  :commands (notmuch
-	     notmuch-search)
   :general
   (my-leader
-    "ai" 'my-inbox)
+    "an" 'notmuch)
   :init
   ;; load config for browse-url-mail, used by x-scheme-handler/mailto
   (with-eval-after-load 'browse-url (require 'notmuch))
@@ -35,21 +21,6 @@
     "f" 'notmuch-show-forward-message
     "r" 'notmuch-show-reply-sender
     "R" 'notmuch-show-reply)
-
-  (evil-define-key 'normal notmuch-message-mode-map
-    (kbd "TAB") 'message-tab)
-
-  ;; TODO: PR evil-collection?
-  (evil-set-initial-state 'notmuch-tree-mode 'normal)
-  (evil-define-key 'visual notmuch-search-mode-map
-    "+" 'notmuch-search-add-tag
-    "-" 'notmuch-search-remove-tag)
-  (evil-define-key 'visual notmuch-show-mode-map
-    "+" 'notmuch-show-add-tag
-    "-" 'notmuch-show-remove-tag)
-  (evil-define-key 'visual notmuch-tree-mode-map
-    "+" 'notmuch-tree-add-tag
-    "-" 'notmuch-tree-remove-tag)
 
   ;; send from multiple accounts with msmtp
   ;; https://notmuchmail.org/emacstips/#index11h2
