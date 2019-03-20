1c379f5b6870e2c6d83648f245c0cd7fc82ab7c3
Jack Kamm <jackkamm@gmail.com>
2019-03-20 15:57:13 +0000

    Revert "Remove exec-path-from-shell"
    This reverts commit a443b8b5fcf45b1cba0a61472c89d4ce33b7e44f.

--- lisp/my-config/my-startup.el
+++ lisp/my-config/my-startup.el
@@ -1,5 +1,9 @@
 (use-package esup :commands esup)
 
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
+
 (use-package keychain-environment
   :config
   (keychain-refresh-environment))
