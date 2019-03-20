a443b8b5fcf45b1cba0a61472c89d4ce33b7e44f
Jack Kamm <jackkamm@gmail.com>
2019-03-20 08:10:40 -0700

    Remove exec-path-from-shell

--- lisp/my-config/my-startup.el
+++ lisp/my-config/my-startup.el
@@ -1,9 +1,5 @@
 (use-package esup :commands esup)
 
-(use-package exec-path-from-shell
-  :config
-  (exec-path-from-shell-initialize))
-
 (use-package keychain-environment
   :config
   (keychain-refresh-environment))
