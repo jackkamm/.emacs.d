f5299141462da8ca6099c613d95b77358c084f2b
Jack Kamm <jackkamm@gmail.com>
2019-03-19 23:21:23 -0700

    Add grep to evil-collection-mode-list

--- lisp/my-config/my-evil-collection.el
+++ lisp/my-config/my-evil-collection.el
@@ -1,5 +1,5 @@
 (use-package evil-collection
   :after evil
-  :custom (evil-collection-mode-list '(term comint))
+  :custom (evil-collection-mode-list '(term comint grep))
   :config
   (evil-collection-init))
