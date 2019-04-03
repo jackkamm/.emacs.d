a3c36f5fac5570c905be87f69d21214a21aeba7b
Jack Kamm <jackkamm@gmail.com>
2019-04-02 19:49:33 -0700

    Simplify company config

--- lisp/my-config/my-company.el
+++ lisp/my-config/my-company.el
@@ -1,18 +1,6 @@
 (use-package company
-  :commands (company-mode company-mode-on)
-  :init
-  (dolist (h (list 'prog-mode-hook
-		   'text-mode-hook))
-    (add-hook h 'company-mode-on))
+  :defer 2
+  :custom
+  (global-company-mode t)
   :config
-  (bind-keys :map company-active-map
-	     ("<return>" . nil))
-
-  (with-eval-after-load "my-ivy"
-    (my-leader "c" 'counsel-company)))
-
-(with-eval-after-load "my-helm"
-  (use-package helm-company
-    :after company
-    :config
-    (my-leader "c" 'helm-company)))
+  (my-leader "c" 'company-complete))
