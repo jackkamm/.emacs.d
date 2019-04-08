091e9ebd8f30fd4f9de1d188554a47edb23133be
Jack Kamm <jackkamm@gmail.com>
2019-04-08 03:07:09 +0000

    Remove ob-ipython

--- lisp/my-config/my-org.el
+++ lisp/my-config/my-org.el
@@ -46,7 +46,7 @@
      (emacs-lisp . t)
      (shell . t)
      (ein . t)
-     (ipython . t)))
+     ))
 
   ;; macro for "C-c ' SPC m b C-c '"
   ;; open block (C-c '), eval buffer (SPC m b), close block (C-c ')
@@ -94,17 +94,6 @@
   :config
   (add-hook 'org-mode-hook 'evil-org-mode)
   (add-hook 'evil-org-mode-hook 'evil-org-set-key-theme))
-
-;; NOTE if jupyter dependencies not installed, will break org-babel in hard to debug ways, even if not using ob-ipython in notebook!
-;; TODO insert some sort of check for jupyter?
-;; TODO replace with https://github.com/dzop/emacs-jupyter
-(use-package ob-ipython :defer t
-  :config
-  (with-eval-after-load 'popwin
-    (push '("^\*ob-ipython-.+\*.+$"
-	    :regexp t
-	    :noselect t)
-	  popwin:special-display-config)))
 
 (use-package ob-async
   :after org
