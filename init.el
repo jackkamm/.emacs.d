5e048262ebed5901077e58db6a7ed4f9ea3604df
Jack Kamm <jackkamm@gmail.com>
2019-05-19 14:54:55 -0700

    Load default extra modules in init.el

--- init.el
+++ init.el
@@ -1,7 +1,40 @@
+;; NOTE ~/.emacs takes precedence over this file;
+;; so to use a different config, copy this file to ~/.emacs and adjust it
+
+;; Bootstrap setup and load "core" modules
 (load (concat user-emacs-directory "lisp/my-init.el"))
 
-;; To load additional modules, copy this file to ~/.emacs,
-;; and add lines like:
-;; (load "my-python")
+;; load extra modules not included in my-init.el
+(mapc 'load
+      (list
+       ;; applications
+       "my-email"
+       "my-irc"
+       "my-tramp"
+       "my-shell"
+       "my-startup" ;path, keychain, profiler
+       "my-docker"
+
+       ;; theming
+       "my-theme"
+       "my-emoji"
+       "my-osx"
+       ;;"my-hidpi"
+
+       ;; languages
+       "my-python"
+       ;;"my-clojure"
+       "my-literate-programming"
+       "my-R"
+       "my-julia"
+       "my-emacs-lisp"
+       "my-tex"
+       "my-web"
+       "my-yaml"
+       ;; c-c++: only enable 1 of cquery, rtags
+       ;;"my-cquery"
+       ;;"my-rtags"
+       "my-lang-misc"
+       ))
 
 (load-theme 'moe-dark t)
