c18ca3481a7383a4256e0e9268436d0633565150
Jack Kamm <jackkamm@gmail.com>
2019-07-21 10:02:11 -0700

    Disable lsp-ui-doc

--- lisp/my-ide.el
+++ lisp/my-ide.el
@@ -2,7 +2,10 @@
 
 (use-package lsp-mode :commands lsp)
 
-(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-doc-enable nil))
 (use-package company-lsp :commands company-lsp)
 
 (with-eval-after-load "my-python" (add-hook 'python-mode-hook #'lsp))
