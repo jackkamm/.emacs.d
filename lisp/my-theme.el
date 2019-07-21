c0c076bc1ec037d3aeceed1f20acece2b5732ba4
Jack Kamm <jackkamm@gmail.com>
2019-07-21 10:00:35 -0700

    Remove eziam submodule (switch back to upstream/melpa version)

--- lisp/my-theme.el
+++ lisp/my-theme.el
@@ -45,7 +45,6 @@
 
 ;; Themes
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/eziam-theme-emacs/")
 (unless (package-installed-p 'moe-theme) (package-install 'moe-theme))
 
 (my-theme-leader
