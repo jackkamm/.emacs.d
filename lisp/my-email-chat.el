1c68c5db40f9a564626cf98a3e12860a2532fa03
Jack Kamm <jackkamm@gmail.com>
2019-08-15 22:59:00 -0700

    erc: simplify config; switch from znc to weechat

--- lisp/my-email-chat.el
+++ lisp/my-email-chat.el
@@ -2,40 +2,13 @@
 
 (use-package erc
   :commands erc
-  :general
-  (my-leader "az" 'my-znc-freenode)
-  :init
-  (defun my-znc-freenode (tunnelp)
-    (interactive
-     (list (y-or-n-p "Open tunnel?")))
-    (when tunnelp
-      (start-process "*znc-port-forward*" "*znc-port-forward*"
-                     "ssh" "-NL" "55555:localhost:55555" "znc"))
-    (erc
-     ;; forward znc to local port 55555
-     :server "localhost" :port "55555"
-     ;; configure SASL on 1st login (https://wiki.znc.in/Sasl)
-     ;; no other configuration should be needed...
-     :nick "freenode"
-     ;; password has format
-     ;;     <znc-user>/freenode:<znc-password>
-     ;; store password in authinfo
-     :password nil))
+  :custom
+  ;; do "ssh -NL 9000:localhost:9000 irc" to open the port
+  ((erc-server "localhost")
+   (erc-port "9000")
+   (erc-nick "snackattack"))
   :config
-  (add-to-list 'erc-modules 'notifications)
-
-  ;; DETACH instead of PART when killing channel buffer
-  ;; see also znc-detach-channel from
-  ;; https://github.com/sshirokov/ZNC.el/blob/master/znc.el
-  (defun my-erc-kill-channel ()
-    (when (erc-server-process-alive)
-      (let ((tgt (erc-default-target)))
-        (if tgt
-            (erc-server-send (format "DETACH %s" tgt)
-                             nil tgt)))))
-
-  (advice-add 'erc-kill-channel
-              :override 'my-erc-kill-channel))
+  (add-to-list 'erc-modules 'notifications))
 
 ;; Mail
 
