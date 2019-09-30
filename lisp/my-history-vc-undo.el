20146ea9d2ebd68f52335e3cefb3f543a73cfc55
Jack Kamm <jackkamm@gmail.com>
2019-09-29 17:39:00 -0700

    undo: undo-propose keybindings

--- lisp/my-history-vc-undo.el
+++ lisp/my-history-vc-undo.el
@@ -3,12 +3,16 @@
 ;; disable undo-tree
 ;; https://www.reddit.com/r/emacs/comments/85t95p/undo_tree_unrecognized_entry_in_undo_list/
 (with-eval-after-load 'evil
-  (global-undo-tree-mode -1))
+  (global-undo-tree-mode -1)
+  (evil-define-key 'normal 'global "u" 'undo-only))
 
 (use-package undo-propose
   :ensure nil
-  :general
-  (my-leader "U" 'undo-propose))
+  :commands undo-propose
+  :init
+  (evil-define-key 'normal 'global (kbd "C-r") 'undo-propose)
+  :config
+  (add-hook 'undo-propose-entry-hook 'undo))
 
 ;; Git
 
