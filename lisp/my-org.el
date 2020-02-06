1487b40e4aa8e68555eaf795e2cebe3ebf503ab7
Jack Kamm <jackkamm@gmail.com>
2020-02-05 17:23:25 -0800

    org: Switch to plain window-setup

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -17,7 +17,7 @@
    org-return-follows-link t
    org-tags-column 0
    org-agenda-tags-column 0
-   org-src-window-setup 'reorganize-frame
+   org-src-window-setup 'plain
 
    ;; https://yiufung.net/post/org-mode-hidden-gems-pt1/
    org-catch-invisible-edits 'show-and-error
