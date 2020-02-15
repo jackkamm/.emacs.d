f46a1d11ddc5975a47d382a7e34af918e7d0336e
Jack Kamm <jackkamm@gmail.com>
2020-02-15 09:53:12 -0800

    org: display and cache remote inline images

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -19,6 +19,7 @@
    org-agenda-tags-column 0
    org-src-window-setup 'plain
    org-table-header-line-p t
+   org-display-remote-inline-images 'cache
 
    ;; https://yiufung.net/post/org-mode-hidden-gems-pt1/
    org-catch-invisible-edits 'show-and-error
