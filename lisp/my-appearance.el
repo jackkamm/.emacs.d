007adbd419591930ef8e1df613954e1b9dd66c63
Jack Kamm <jackkamm@gmail.com>
2020-02-26 21:12:16 -0800

    appearance: disable global-hl-line-mode

--- lisp/my-appearance.el
+++ lisp/my-appearance.el
@@ -17,8 +17,8 @@
   :general
   (my-leader "tM" 'minimap-mode))
 
-;; make the cursor more visible
-(global-hl-line-mode 1)
+;;;; make the cursor more visible
+;;(global-hl-line-mode 1)
 
 ;; pos-tip
 (use-package pos-tip)
