4ccd6ecbfd59c542ef8e4aac8d7f35f122d579be
Jack Kamm <jackkamm@gmail.com>
2020-07-03 13:59:01 -0700

    undo: Backport emacs28 redo

--- lisp/my-history-vc-undo.el
+++ lisp/my-history-vc-undo.el
@@ -15,6 +15,34 @@
 ;;  :general
 ;;  (my-leader "U" 'undo-propose))
 
+;; Backport of emacs28 undo-redo
+
+(defun my-undo-redo (&optional arg)
+  "Undo the last ARG undos."
+  (interactive "*p")
+  (cond
+   ((not (my-undo--last-change-was-undo-p buffer-undo-list))
+    (user-error "No undo to undo"))
+   (t
+    (let* ((ul buffer-undo-list)
+           (new-ul
+            (let ((undo-in-progress t))
+              (while (and (consp ul) (eq (car ul) nil))
+                (setq ul (cdr ul)))
+              (primitive-undo arg ul)))
+           (new-pul (my-undo--last-change-was-undo-p new-ul)))
+      (message "Redo%s" (if undo-in-region " in region" ""))
+      (setq this-command 'undo)
+      (setq pending-undo-list new-pul)
+      (setq buffer-undo-list new-ul)))))
+
+(defun my-undo--last-change-was-undo-p (undo-list)
+  (while (and (consp undo-list) (eq (car undo-list) nil))
+    (setq undo-list (cdr undo-list)))
+  (gethash undo-list undo-equiv-table))
+
+(evil-define-key 'normal 'global (kbd "C-r") 'my-undo-redo)
+
 ;; Git
 
 (my-leader "g" '(:ignore t :which-key "Git"))
