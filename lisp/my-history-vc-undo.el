5e0688499e115f3cbcc5acfbf079158399875139
Jack Kamm <jackkamm@gmail.com>
2020-07-17 23:11:48 -0700

    undo: use undo-redo from emacs28

--- lisp/my-history-vc-undo.el
+++ lisp/my-history-vc-undo.el
@@ -4,7 +4,8 @@
 ;; https://www.reddit.com/r/emacs/comments/85t95p/undo_tree_unrecognized_entry_in_undo_list/
 (with-eval-after-load 'evil
   (global-undo-tree-mode -1)
-  (evil-define-key 'normal 'global "u" 'undo-only))
+  (evil-define-key 'normal 'global "u" 'undo-only)
+  (evil-define-key 'normal 'global (kbd "C-r") 'undo-redo))
 
 ;; access undo-only from visual/insert state
 (define-key global-map (kbd "C-x u") 'undo-only)
@@ -14,34 +15,6 @@
 ;;  :ensure nil
 ;;  :general
 ;;  (my-leader "U" 'undo-propose))
-
-;; Backport of emacs28 undo-redo
-
-(defun my-undo-redo (&optional arg)
-  "Undo the last ARG undos."
-  (interactive "*p")
-  (cond
-   ((not (my-undo--last-change-was-undo-p buffer-undo-list))
-    (user-error "No undo to undo"))
-   (t
-    (let* ((ul buffer-undo-list)
-           (new-ul
-            (let ((undo-in-progress t))
-              (while (and (consp ul) (eq (car ul) nil))
-                (setq ul (cdr ul)))
-              (primitive-undo arg ul)))
-           (new-pul (my-undo--last-change-was-undo-p new-ul)))
-      (message "Redo%s" (if undo-in-region " in region" ""))
-      (setq this-command 'undo)
-      (setq pending-undo-list new-pul)
-      (setq buffer-undo-list new-ul)))))
-
-(defun my-undo--last-change-was-undo-p (undo-list)
-  (while (and (consp undo-list) (eq (car undo-list) nil))
-    (setq undo-list (cdr undo-list)))
-  (gethash undo-list undo-equiv-table))
-
-(evil-define-key 'normal 'global (kbd "C-r") 'my-undo-redo)
 
 ;; Git
 
