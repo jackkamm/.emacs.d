d07b378c77065d844dfe79e328a2c9dbfc01b378
Jack Kamm <jackkamm@gmail.com>
2021-03-05 19:28:42 -0800

    config-template: Fix org example

--- config-template.el
+++ config-template.el
@@ -1,10 +1,12 @@
 ;;;; Load desired version of org-mode first to avoid any conflicts
 ;;(use-package org
 ;;  ;;;; local dev version
+;;  ;;:ensure nil
 ;;  ;;:load-path ("~/Documents/org-mode/lisp"
 ;;  ;;            "~/Documents/org-mode/contrib/lisp")
 ;;  ;; melpa version
-;;  :ensure org-plus-contrib)
+;;  :ensure org-plus-contrib
+;;  :demand t)
 
 (load "my-base")
 ;;(load "my-config")
