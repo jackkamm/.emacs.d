8627b1f4d1e0275cf352f537686f7ce4bdbbd74c
Jack Kamm <jackkamm@gmail.com>
2021-03-09 15:04:38 -0800

    org: Switch back to 3 priorities A-C. Adjust agenda sorting strategy

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -32,10 +32,6 @@
   ;; to resize inline images
   (org-image-actual-width '(750))
 
-  (org-priority-highest ?A)
-  (org-priority-lowest ?Z)
-  (org-priority-default ?M)
-
   (org-link-file-path-type
    (lambda (path)
      (let* ((proj (project-current))
@@ -67,6 +63,19 @@
 
   (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 
+  (org-agenda-cmp-user-defined
+   (lambda (a b)
+     "Custom implementation of sorting by priority. Workaround
+     due to problems in the default implementation. See also:
+
+     https://orgmode.org/list/CAOkDyE9ZjdN4Me4Pi3poC3C4q8c=ivdWz2uqiWHrYfe8jmF3ZQ@mail.gmail.com/T/#m2385e4682c8d060d056c728f803f5b080e75b6c1
+"
+     (let ((pa (org-get-priority a))
+           (pb (org-get-priority b)))
+       (cond ((< pa pb) -1)
+             ((> pa pb) 1)
+             (t nil)))))
+
   ;; NOTE org-reverse-note-order is bugged: if file starts with
   ;; section header, refiling to top-level is incorrectly inserted
   ;; TODO minimal reproducible example + bug report
@@ -82,6 +91,11 @@
     (interactive)
     (org-set-property "CREATED" (format-time-string "[%Y-%m-%d]")))
   :config
+  (with-eval-after-load 'org-agenda
+    (add-to-list 'org-agenda-sorting-strategy
+                 '(agenda habit-down time-up user-defined-down
+                          deadline-down scheduled-down category-keep)))
+
   ;; Disable auto-indentation
   (customize-set-variable 'org-adapt-indentation nil)
   (add-hook 'org-mode-hook (lambda () (setq evil-auto-indent nil)))
