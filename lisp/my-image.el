d6dbb461dc7207c643d0885286aa355577d58cc7
Jack Kamm <jackkamm@gmail.com>
2024-06-06 09:15:02 -0700

    my-copy-image: Don't ask for encoding when copying

--- lisp/my-image.el
+++ lisp/my-image.el
@@ -15,28 +15,29 @@
 
 ;; Originally based on ox-clip-image-to-clipboard
 (defun my-copy-image (&optional scale)
-  "To be run within image-mode. Works with tramp"
+  "To be run within image-mode. Works with tramp, and even for
+images without a backing file (if that can happen?)"
   (interactive "P")
   (let* ((contents (buffer-substring-no-properties (point-min) (point-max)))
-         (image-file (make-temp-file "my-copy-image")))
-    (with-temp-file image-file
+         (tmp-file (make-temp-file "my-copy-image"))
+         (coding-system-for-write 'raw-text))
+    (with-temp-file tmp-file
       (insert contents))
     (cond
      ((eq system-type 'windows-nt)
       (message "Not supported yet."))
      ((eq system-type 'darwin)
       (do-applescript
-       (format "set the clipboard to POSIX file \"%s\"" (expand-file-name image-file))))
+       (format "set the clipboard to POSIX file \"%s\"" (expand-file-name tmp-file))))
      ((eq system-type 'gnu/linux)
       (call-process-shell-command
        (format "xclip -selection clipboard -t image/%s -i %s"
-	       (file-name-extension image-file)
-	       image-file))))
-    (message "Copied %s" image-file)
-    ;; NOTE removing the tmp file can cause race condition with
-    ;; copying to clipboard. Add a sleep-for here?
-    ;;(delete-file image-file)
-    ))
+               ;; FIXME tmp-file has no extension
+	       (file-name-extension tmp-file)
+	       tmp-file))))
+    (message "Copied %s" tmp-file)
+    (sleep-for 100)
+    (delete-file tmp-file)))
 
 (provide 'my-image)
 ;;; my-image.el ends here
